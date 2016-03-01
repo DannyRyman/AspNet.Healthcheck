102328814f20f9c42e3d09951ddec52e6c8e4208
danielr <danielr@digiterre.com>
2016-03-01 14:48:27 +0000

    Add extensions

---
+++ src/AspNetHealthcheck/HealthCheckExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Http;
+
+namespace AspNetHealthCheck
+{
+    public static class HealthCheckExtensions
+    {
+        private const string DefaultRouteTemplate = "healthcheck";
+
+        public static void EnableHealthChecks(
+            this HttpConfiguration httpConfig,
+            string applicationVersion = null,
+            Action<HealthCheckConfig> configure = null)
+        {
+            EnableHealthChecks(httpConfig, DefaultRouteTemplate, applicationVersion, configure);
+        }
+
+        public static void EnableHealthChecks(
+            this HttpConfiguration httpConfig,
+            string routeTemplate,
+            string applicationVersion = null,
+            Action<HealthCheckConfig> configure = null
+            )
+        {
+            var config = new HealthCheckConfig();
+            if (configure != null) configure(config);
+
+            httpConfig.Routes.MapHttpRoute(
+                name: "health_check",
+                routeTemplate: routeTemplate,
+                defaults: null,
+                constraints: null,
+                handler: new HealthCheckHandler(config, applicationVersion)
+                );
+        }
+
+    }
+}
